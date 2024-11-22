ac6c767081bf912afddff70012dd2d7f219f3a31
Kyle <kyou4@my.bcit.ca>
2024-11-21 17:25:10 -0800

    update

--- startingcode/IOhandler.js
+++ startingcode/IOhandler.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-// const PNG = require("pngjs").PNG;
+const PNG = require("pngjs").PNG;
 const path = require("path");
 const yauzl = require("yauzl-promise");
 const { pipeline } = require("stream/promises");
@@ -10,35 +10,51 @@ const { pipeline } = require("stream/promises");
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = async (pathIn, pathOut) => {
-  const zippedFolder = __dirname + `\\${pathIn}`;
-  const unzippedFolder = __dirname + `\\${pathOut}`;
-  const zip = await yauzl.open(zippedFolder);
 
+const unzip = async (pathIn, pathOut) => {
+  const zip = await yauzl.open(pathIn);
   try {
+    await fs.promises.mkdir(pathOut, { recursive: true });
     for await (const entry of zip) {
-      console.log(entry);
       if (entry.filename.endsWith("/")) {
+        await fs.promises.mkdir(path.join(pathOut, entry.filename), {
+          recursive: true,
+        });
+      } else {
         const readStream = await entry.openReadStream();
         const writeStream = fs.createWriteStream(
-          unzippedFolder + `\\${entry.filename}`
+          path.join(pathOut, entry.filename)
         );
         await pipeline(readStream, writeStream);
       }
     }
+  } catch (error) {
+    console.log(error);
   } finally {
     await zip.close();
+    console.log("Extraction operation complete");
   }
 };
-unzip("myfile.zip", "output");
+
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
  *
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
-
+const readDir = async (dir) => {
+  try {
+    const files = await fs.promises.readdir(dir);
+    const filteredFile = files.filter(
+      (element) => path.extname(element) === ".png"
+    );
+    return filteredFile;
+    // const photoPath = filteredFile.map((element) => path.join(dir, element));
+    // return photoPath;
+  } catch (error) {
+    console.log(error);
+  }
+};
 /**
  * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
@@ -47,10 +63,72 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = async (pathIn, pathOut) => {
+  try {
+    fs.createReadStream(pathIn)
+      .pipe(new PNG())
+      .on("parsed", function () {
+        for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
+            const gray =
+              (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+
+            this.data[idx] = gray;
+            this.data[idx + 1] = gray;
+            this.data[idx + 2] = gray;
+          }
+        }
+
+        this.pack().pipe(fs.createWriteStream(pathOut));
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const sepia = async (pathIn, pathOut) => {
+  try {
+    fs.createReadStream(pathIn)
+      .pipe(new PNG())
+      .on("parsed", function () {
+        for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
+
+            let newRed =
+              this.data[idx] * 0.393 +
+              this.data[idx + 1] * 0.769 +
+              this.data[idx + 2] * 0.189;
+            let newGreen =
+              this.data[idx] * 0.349 +
+              this.data[idx + 1] * 0.686 +
+              this.data[idx + 2] * 0.168;
+            let newBlue =
+              this.data[idx] * 0.272 +
+              this.data[idx + 1] * 0.534 +
+              this.data[idx + 2] * 0.131;
+
+            if (newRed > 255) newRed = 255;
+            if (newGreen > 255) newGreen = 255;
+            if (newBlue > 255) newBlue = 255;
+
+            this.data[idx] = newRed;
+            this.data[idx + 1] = newGreen;
+            this.data[idx + 2] = newBlue;
+          }
+        }
+
+        this.pack().pipe(fs.createWriteStream(pathOut));
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   unzip,
   readDir,
   grayScale,
+  sepia,
 };
--- startingcode/main.js
+++ startingcode/main.js
@@ -1,6 +1,23 @@
 const path = require("path");
-
 const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
+const pathProcessedSepia = path.join(__dirname, "sepia");
+
+const main = async () => {
+  try {
+    await IOhandler.unzip(zipFilePath, pathUnzipped);
+    const files = await IOhandler.readDir(pathUnzipped);
+    for (let picture of files) {
+      await IOhandler.grayScale(
+        path.join(pathUnzipped, picture),
+        path.join(pathProcessed, picture)
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
